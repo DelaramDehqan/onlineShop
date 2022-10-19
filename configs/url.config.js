6aa1a004844852714794b5dd2adf06e3db0a02f1
DelaramDehqan <103545728+DelaramDehqan@users.noreply.github.com>
2022-10-19 16:35:54 +0330

    change directory

--- configs/url.config.js
+++
@@ -1,4 +0,0 @@
-// USER
-export const LOGIN = '/auth/login';
-export const REFRESH_TOKEN = '/auth/refresh-token';
-export const WHOAMI = '/whoami';
--- configs/variables.config.js
+++
@@ -1,1 +0,0 @@
-export const ACCESS_TOKEN = 'ACCESS_TOKEN';export const BASE_URL = process.env.REACT_APP_BASE_URL || 'https://localhost:3001';export const IS_LOGGED_IN = 'IS_LOGGED_IN';export const REFRESH_TOKEN = 'REFRESH_TOKEN';
