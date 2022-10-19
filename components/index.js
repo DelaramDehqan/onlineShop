30c49d0ebe06909fbff0f3eebf76093fb3625b7e
DelaramDehqan <103545728+DelaramDehqan@users.noreply.github.com>
2022-10-19 16:35:40 +0330

    change directory

--- components/index.js
+++
@@ -1,6 +0,0 @@
-export {Navigation} from './Navigation/Navigation.component';
-export {ProductCard} from './productCard/ProductCard.component'
-export {SearchInput} from './Search/SeachInput.component'
-export {Sidebar} from './Sidebar/Sidebar.Component'
-export { Spinner } from './Spinner/Spinner.component';
-export { Banner } from "./Banner/index";
