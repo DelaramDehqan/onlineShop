9c35c7f7d2298e50aaa430e5960feb33ef7eacc8
DelaramDehqan <103545728+DelaramDehqan@users.noreply.github.com>
2022-10-19 16:37:05 +0330

    change directory

--- routes/components/ProtectedRoute/ProtectedRoute.config.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const DEFAULT_PROPS = {
-
-};
-
-export const PROP_TYPES = {
-  component: PropTypes.func.isRequired,
-};
--- routes/components/index.js
+++
@@ -1,4 +0,0 @@
-export {History} from './History/History.component';
-export {PrivateRoute} from './PrivateRoute/PrivateRoute.component';
-export {ProtectedRoute} from './ProtectedRoute/ProtectedRoute.component';
-export {PublicRoute} from './PublicRoute/PublicRoute.component';
